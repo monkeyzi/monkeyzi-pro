b824baab8f083413ee927f8cb0c4c73478c98b36
854152531@qq.com <854152531@qq.com>
2019-02-27 21:59:39 +0800

    ceshi

---
+++ monkeyzi-pro-monitor/src/main/java/com/monkeyzi/mcloud/monitor/MonkeyziMonitorApplication.java
@@ -0,0 +1,19 @@
+package com.monkeyzi.mcloud.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author: 高yg
+ * @date: 2019/2/27 21:50
+ * @qq:854152531@qq.com
+ * @blog http://www.monkeyzi.xin
+ * @description:
+ */
+@SpringCloudApplication
+public class MonkeyziMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonkeyziMonitorApplication.class);
+    }
+}
